648bb5b5d5a7ed50e888638547cd5b236fd65b10
annielz <lmlmzhen@sina.com>
2023-05-18 19:57:50 +0800

    feat: metadata refactor init
    feat: migrate all metadata methods clients and services
    
    add getObjects handler

--- modular/gater/router.go
+++ modular/gater/router.go
@@ -72,20 +72,18 @@ func (g *GateModular) RegisterHandler(router *mux.Router) {
 			model.StartTimestampUs, "{start_ts}",
 			model.EndTimestampUs, "{end_ts}").
 		HandlerFunc(g.listBucketReadRecordHandler)
-	//hostBucketRouter.NewRoute().
-	//	Name(listObjectsByBucketRouterName).
-	//	Methods(http.MethodGet).
-	//	Path("/").
-	//	HandlerFunc(g.listObjectsByBucketNameHandler)
+	hostBucketRouter.NewRoute().
+		Name(listObjectsByBucketRouterName).
+		Methods(http.MethodGet).
+		Path("/").
+		HandlerFunc(g.listObjectsByBucketNameHandler)
 	hostBucketRouter.NotFoundHandler = http.HandlerFunc(g.notFoundHandler)
 
-	// bucket list router, virtual-hosted style
-	//bucketListRouter := r.Host(g.config.Domain).Subrouter()
-	//bucketListRouter.NewRoute().
-	//	Name(getUserBucketsRouterName).
-	//	Methods(http.MethodGet).
-	//	Path("/").
-	//	HandlerFunc(g.getUserBucketsHandler)
+	// bucket list router, path style
+	router.Path("/").
+		Name(getUserBucketsRouterName).
+		Methods(http.MethodGet).
+		HandlerFunc(g.getUserBucketsHandler)
 
 	// admin router, path style
 	router.Path(model.GetApprovalPath).
@@ -152,11 +150,11 @@ func (g *GateModular) RegisterHandler(router *mux.Router) {
 			model.StartTimestampUs, "{start_ts}",
 			model.EndTimestampUs, "{end_ts}").
 		HandlerFunc(g.listBucketReadRecordHandler)
-	//pathBucketRouter.NewRoute().
-	//	Name(listObjectsByBucketRouterName).
-	//	Methods(http.MethodGet).
-	//	Path("/").
-	//	HandlerFunc(g.listObjectsByBucketNameHandler)
+	pathBucketRouter.NewRoute().
+		Name(listObjectsByBucketRouterName).
+		Methods(http.MethodGet).
+		Path("/").
+		HandlerFunc(g.listObjectsByBucketNameHandler)
 	pathBucketRouter.NotFoundHandler = http.HandlerFunc(g.notFoundHandler)
 
 	router.NotFoundHandler = http.HandlerFunc(g.notFoundHandler)
